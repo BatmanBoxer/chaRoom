2759b20ed7b983eb03daa834f519ef0202b256a3
darwin <darwinkoirala123@gmail.com>
2024-10-11 00:28:50 +0545

    fixed compliing issue of gradle in fedora

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,9 @@ plugins {
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
-    kotlin("plugin.serialization") version "1.9.10"
+    kotlin("plugin.serialization") version "2.0.0"
     id("com.google.gms.google-services")
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
